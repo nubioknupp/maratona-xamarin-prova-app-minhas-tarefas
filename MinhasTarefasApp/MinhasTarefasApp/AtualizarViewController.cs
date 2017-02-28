8c673e6cd518e70f299ac98d3910f94aac5e9edd
Núbio Knupp <nubioknupp@gmail.com>
2017-02-28 18:29:06 -0300

    Adicionei um View Controller para editar tarefa.

---
+++ MinhasTarefasApp/MinhasTarefasApp/AtualizarViewController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+using UIKit;
+
+namespace MinhasTarefasApp
+{
+	public partial class AtualizarViewController : UIViewController
+	{
+		protected AtualizarViewController(IntPtr handle) : base(handle)
+		{
+			// Note: this .ctor should not contain any initialization logic.
+		}
+
+		public override void ViewDidLoad()
+		{
+			base.ViewDidLoad();
+			// Perform any additional setup after loading the view, typically from a nib.
+			///AdicionarButton.TouchUpInside += AdicionarButton_TouchUpInside;
+		
+		}
+
+		public override void DidReceiveMemoryWarning()
+		{
+			base.DidReceiveMemoryWarning();
+			// Release any cached data, images, etc that aren't in use.
+		}
+
+		protected void AdicionarButton_TouchUpInside(object sender, EventArgs e)
+		{
+			new UIAlertView("Touch3", "TouchUpInside handled", null, "OK", null).Show();
+		}
+	}
+}
+
