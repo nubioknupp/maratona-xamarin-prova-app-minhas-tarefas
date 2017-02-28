047566484f81d7b51d0fd4e84c33013afa664094
Núbio Knupp <nubioknupp@gmail.com>
2017-02-28 18:29:06 -0300

    Revert "Adicionei um View Controller para editar tarefa."
    This reverts commit 8c673e6cd518e70f299ac98d3910f94aac5e9edd.

--- MinhasTarefasApp/MinhasTarefasApp/MainTableController.cs
+++ MinhasTarefasApp/MinhasTarefasApp/MainTableController.cs
@@ -1,15 +1,10 @@
 ﻿using System;
 using UIKit;
-using System.Linq;
-using System.Collections;
-using Foundation;
 
 namespace MinhasTarefasApp
 {
 	public partial class MainTableController : UITableViewController
 	{
-		private ArrayList lstTarefa = new ArrayList();
-
 		protected MainTableController(IntPtr handle) : base(handle)
 		{
 			// Note: this .ctor should not contain any initialization logic.
@@ -19,9 +14,6 @@ namespace MinhasTarefasApp
 		{
 			base.ViewDidLoad();
 			// Perform any additional setup after loading the view, typically from a nib.
-
-			lstTarefa.Add("Teste 1");
-			lstTarefa.Add("Teste 2");
 		}
 
 		public override void DidReceiveMemoryWarning()
@@ -30,58 +22,24 @@ namespace MinhasTarefasApp
 			// Release any cached data, images, etc that aren't in use.
 		}
 
-		public override void CommitEditingStyle(UITableView tableView, UITableViewCellEditingStyle editingStyle, NSIndexPath indexPath)
-		{
-			if (editingStyle == UITableViewCellEditingStyle.Delete)
-			{
-				Console.WriteLine("Teste Delete");
-			}
-		}
 
-		//
+		//public override nint NumberOfSections(UITableView tableView)
+		//{
+		//	return nint.Parse("1");
+		//}
 
-		public override void PrepareForSegue(UIStoryboardSegue segue, NSObject sender)
-		{
-			if (segue.Identifier == "AdicionarTarefaSegue")
-			{
-				//var detail = segue.DestinationViewController as DetailViewController;
+		//public override UITableViewCell GetCell(UITableView tableView, NSIndexPath indexPath)
+		//{
+		//	var myCell = (Cell)tableView.DequeueReusableCell("celulaTarefaId");
+		//	myCell.SetCell(ListNewses[indexPath.Row].Title.ToString(),
+		//						ListNewses[indexPath.Row].Description.ToString());
+		//	return myCell;
+		//}
 
-				//if (detail != null)
-				//{
-				//	var sourceNews = tableNews.Source as TableSourceNews;
-				//	var rowPathNews = tableNews.IndexPathForSelectedRow;
-				//	var news = sourceNews.GetNews(rowPathNews.Row);
-				//	detail.SetNews(news);
-				//	newsNav.BackBarButtonItem.Title = "";
-				//}
-
-				Console.WriteLine("AdicionarTarefaSegue");
-			} 
-			else if (segue.Identifier == "EditarTarefaSegue") 
-			{ 
-			    Console.WriteLine("EditarTarefaSegue");
-			}
-
-			base.PrepareForSegue(segue, sender);
-		}
-
-
-		public override nint NumberOfSections(UITableView tableView)
-		{
-			return nint.Parse("1");
-		}
-
-		public override UITableViewCell GetCell(UITableView tableView, Foundation.NSIndexPath indexPath)
-		{
-			var celula = tableView.DequeueReusableCell("celulaTarefaId");
-			celula.TextLabel.Text = lstTarefa[indexPath.Row].ToString();
-			return celula;
-		}
-
-		public override nint RowsInSection(UITableView tableview, nint section)
-		{
-			return nint.Parse(lstTarefa.Count.ToString());
-		}
+		//public override nint RowsInSection(UITableView tableview, nint section)
+		//{
+		//	return nint.Parse(ListNewses.Count.ToString());
+		//}
 
 	}
 }
